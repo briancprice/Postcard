2a14d4f2fd272aa5998da339fd9fc113873a6037
Brian Price <briancprice@users.noreply.github.com>
2014-10-09 16:33:48 -0400

    first rev of project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -9,17 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Label for recipient's name...
+    @IBOutlet weak var messageLabel: UILabel!;
+    
+    // Recipient's name text field...
+    @IBOutlet weak var enterNameTextField: UITextField!;
 
+    // Message text field
+    @IBOutlet weak var enterMessageTextField: UITextField!;
+    
+    // Mail Button field
+    @IBOutlet weak var mailButton: UIButton!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        super.didReceiveMemoryWarning();
         // Dispose of any resources that can be recreated.
     }
 
+    // When pressed, shows the messageLabel and updates the text
+    // to that of the enterNameTextField
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false;
+        messageLabel.text = enterMessageTextField.text;
+        messageLabel.textColor = UIColor.redColor();
+        
+        
+        enterMessageTextField.text = "";
+        enterMessageTextField.resignFirstResponder();
+        
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal);
+        
+    }
 
 }
 
